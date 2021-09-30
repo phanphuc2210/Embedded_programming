47e74900dd4b7901bf91e3eb4277cc1a2d108ba2
phanphuc2210 <phucpth2001@gmail.com>
2021-09-30 14:23:56 +0700

    Add Bai4_LedSangDan

---
+++ Bai4_LedSangDan_PTHP/LedSangDan.ino
@@ -0,0 +1,18 @@
+int brightness = 0;
+
+void setup()
+{
+  pinMode(9, OUTPUT);
+}
+
+void loop()
+{
+  for (brightness = 0; brightness <= 255; brightness += 5) {
+    analogWrite(9, brightness);
+    delay(30); // Đợi trong 30 mili giây
+  }
+  for (brightness = 255; brightness >= 0; brightness -= 5) {
+    analogWrite(9, brightness);
+    delay(30); // Đợi trong 30 mili giây
+  }
+}
---
+++ Bai4_LedSangDan_PTHP/Readme.txt
@@ -0,0 +1,2 @@
+Đây là bài thực hành lập trình arduino làm cho đèn led sáng dần trong 30 mili giây và tối dần lại trong 30 mili giây liên tục.
+Led được đấu ở chân ~9.
