0710146d9c0730ee8ce1a3ee61f7fdf94e7f43a4
phanphuc2210 <phucpth2001@gmail.com>
2021-10-02 16:49:21 +0700

    update

--- Bai3_CamBienNhietDo_PTHP/CamBienNhietDo.ino
+++ Bai3_CamBienNhietDo_PTHP/CamBienNhietDo.ino
@@ -6,7 +6,6 @@ int temp[5]; // Lấy ra những giá trị nhiệt độ tốt.
 
 void setup()
 {
-   pinMode(A0, INPUT);
    pinMode(10, OUTPUT);
   
 }
@@ -15,19 +14,19 @@ void loop()
 {
   // Chuyển đổi giá trị đọc được từ chân A0 thành 
   //nhiệt độ tương ứng
-  temp[0] = map(analogRead(A0),0, 1023, 0, 500); 
+  temp[0] = map(analogRead(A0),20,358,-40,125); 
   delay(40); // Đợi 40 mili giây để lấy giá trị tiếp theo
     
-  temp[1] = map(analogRead(A0),0, 1023, 0, 500); 
+  temp[1] = map(analogRead(A0),20,358,-40,125); 
   delay(40); // Đợi 40 mili giây để lấy giá trị tiếp theo
     
-  temp[2] = map(analogRead(A0),0, 1023, 0, 500); 
+  temp[2] = map(analogRead(A0),20,358,-40,125); 
   delay(40); // Đợi 40 mili giây để lấy giá trị tiếp theo
     
-  temp[3] = map(analogRead(A0),0, 1023, 0, 500); 
+  temp[3] = map(analogRead(A0),20,358,-40,125); 
   delay(40); // Đợi 40 mili giây để lấy giá trị tiếp theo
     
-  temp[4] = map(analogRead(A0),0, 1023, 0, 500);
+  temp[4] = map(analogRead(A0),20,358,-40,125);
     
   // Lấy nhiệt độ trung bình  
   temperature = (temp[0]+temp[1]+temp[2]+temp[3]+temp[4])/5; 
@@ -35,6 +34,6 @@ void loop()
   if(temperature > 37) {digitalWrite(10,HIGH);}
   else {digitalWrite(10,LOW);}
   
-  delay(500);
+  delay(100);
   
 }
